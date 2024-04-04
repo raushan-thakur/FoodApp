7e83fd65835f85b7d1507b85ed0ed109c462f533
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-04-04 17:23:55 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -19,12 +19,7 @@ const Body = () => {
 
   async function getRestaurant() {
     const data = await fetch(
-      "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7494933&lng=77.11836120000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
-      {
-        headers: {
-        'x-cors-api-key': 'temp_b722fb498d04c59c70b85ba72de25333'
-        }
-      }
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7494933&lng=77.11836120000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       );
     const json = await data.json();
     // console.log(json);
