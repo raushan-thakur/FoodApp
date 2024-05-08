e44c480b5a44bd58500dd42e347a87960fd083cf
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-05-08 11:11:52 +0530

    fixed cors issue

--- src/components/Body.js
+++ src/components/Body.js
@@ -19,7 +19,7 @@ const Body = () => {
 
   async function getRestaurant() {
     const data = await fetch(
-      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7494933&lng=77.11836120000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://cors-handlers.vercel.app/api/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7494933&lng=77.11836120000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       );
     const json = await data.json();
     // console.log(json);
