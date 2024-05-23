d94c6704e216e4d1d289dc35541a8ec1d98bab16
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-05-23 10:03:01 +0530

    cors issue fixed constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@
 
 export const IMG_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const FETCH_MENU_URL = "https://cors-handlers.vercel.app/api/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7494933&lng=77.11836120000001&restaurantId=";
+export const FETCH_MENU_URL = "https://cors-handlers.vercel.app/api/?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D28.7494933%26lng%3D77.11836120000001%26restaurantId%3D";
 
 // export const restaurantList = [
 //     {
